5a07f8e1074d368baa5ac56b668ddeedc5fda9c7
tivdli <timocasper@msn.com>
2021-11-20 13:08:16 +0100

    added js minifier, also included bugs

--- main/minifier.py
+++ main/minifier.py
@@ -1,13 +1,14 @@
 import htmlmin
 import os
-
+from slimit import minify
 dir = os.path.dirname(os.path.realpath(__file__))
 readfile = os.path.join(dir, "index.html")
-writefile = os.path.join(dir, "site.txt")
+writefile = os.path.join(dir, "data", "index.html")
 htmlfile= open(readfile, "r")
 text = htmlfile.read()
 htmlfile.close()
 print(len(text)//1024)
+#out = minify(text, mangle=True)
 out = (htmlmin.minify(text, remove_comments=True, remove_optional_attribute_quotes=True, reduce_boolean_attributes=True, remove_empty_space=True))
 print(len(out)//1024)
 writefile = open(writefile, "w")
