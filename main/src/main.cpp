27f8683090bd7554fd6f706b84a6d61ca7cd4968
tivdli <timocasper@msn.com>
2021-12-02 12:21:16 +0100

    Moved AIGB to comments to focus on GBINT

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -23,7 +23,7 @@ AsyncWebServer server(PORT);
 AsyncWebSocket websocket("/ws");
 AsyncEventSource events("/events");
 GBINT gbint(&server, &websocket, &events);
-AIGB aigb;
+// AIGB aigb;
 
 int temp_reading_inside = 0;
 int temp_reading_outside = 0;
@@ -86,7 +86,7 @@ void setup()
 
     
     gbint.init();
-    aigb.init();
+    // aigb.init();
     //define baud rate for co2 sensor
     
   
@@ -95,7 +95,7 @@ void setup()
 void loop()
 {
     
-    aigb.LED();
-    temp_reading_inside,Hum_reading_inside =aigb.Measurment_In();
+    // aigb.LED();
+    // temp_reading_inside,Hum_reading_inside =aigb.Measurment_In();
     websocket.cleanupClients();
 }
