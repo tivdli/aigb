2ff097d3ad1e7d35886fec9415db46350ab7a007
tivdli <timocasper@msn.com>
2022-01-24 19:53:13 +0100

    fout

--- main/src/aigb.cpp
+++ main/src/aigb.cpp
@@ -1,253 +1,253 @@
 #include <Arduino.h>
 #include <aigb.h>
-#include <ESP32Servo.h>
-#include <data.h>
-#include <AM2320.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_AM2320.h>
-#include <SoftwareSerial.h>
-#include <SPI.h> 
-#include <Adafruit_NeoPixel.h>
+// #include <ESP32Servo.h>
+ #include <data.h>
+// #include <AM2320.h>
+// #include <Adafruit_Sensor.h>
+// #include <Adafruit_AM2320.h>
+// #include <SoftwareSerial.h>
+// #include <SPI.h> 
+// #include <Adafruit_NeoPixel.h>
 // #include <MHZ19PWM.h>
 
-SoftwareSerial AM2320Serial(SDA, SCL);
-/* To Do
-Logica
+// SoftwareSerial AM2320Serial(SDA, SCL);
+// /* To Do
+// Logica
 
- */
+//  */
 
 AIGB::AIGB(DATA * data){
-    init();
+    //init();
     AIGB::aigb_data = data;
 }
 
-void AIGB::init(){
-    //makes four PWM signals for the servos
-    int ADS_max=4096;
-    ESP32PWM::allocateTimer(0);
-    ESP32PWM::allocateTimer(1);
-    ESP32PWM::allocateTimer(2);
-    ESP32PWM::allocateTimer(3);
-    //making two servo objects
-    Servo MyServo1;
-    Servo MyServo2;
-    MyServo1.attach(Servo_1,500,2400);
-    MyServo2.attach(Servo_2,0,180);
-    // making am2320 sensor object
+// void AIGB::init(){
+//     //makes four PWM signals for the servos
+//     int ADS_max=4096;
+//     ESP32PWM::allocateTimer(0);
+//     ESP32PWM::allocateTimer(1);
+//     ESP32PWM::allocateTimer(2);
+//     ESP32PWM::allocateTimer(3);
+//     //making two servo objects
+//     Servo MyServo1;
+//     Servo MyServo2;
+//     MyServo1.attach(Servo_1,500,2400);
+//     MyServo2.attach(Servo_2,0,180);
+//     // making am2320 sensor object
     
-    AM2320 th_1;
-    AM2320 th_2;
-    // defined all the outputs
-    pinMode(Led_pin,OUTPUT);
-    pinMode(Vernevelaar,OUTPUT);
-    pinMode(Led_B,OUTPUT);
-    pinMode(Led_G,OUTPUT);
-    //pinMode(Led_R,OUTPUT);
-    pinMode(CO2_PWM,INPUT);
-    pinMode(Pomp_Voeding,OUTPUT);
-    pinMode(Pomp_Water,OUTPUT);
-    pinMode(Fan_Control_1,OUTPUT);
-    pinMode(Fan_Control_2,OUTPUT);
-    pinMode(Peltier_1,OUTPUT);
-    pinMode(Peltier_2,OUTPUT);
-    pinMode(SDA,OUTPUT);
-    pinMode(SCL,OUTPUT);
+//     AM2320 th_1;
+//     AM2320 th_2;
+//     // defined all the outputs
+//     pinMode(Led_pin,OUTPUT);
+//     pinMode(Vernevelaar,OUTPUT);
+//     pinMode(Led_B,OUTPUT);
+//     pinMode(Led_G,OUTPUT);
+//     //pinMode(Led_R,OUTPUT);
+//     pinMode(CO2_PWM,INPUT);
+//     pinMode(Pomp_Voeding,OUTPUT);
+//     pinMode(Pomp_Water,OUTPUT);
+//     pinMode(Fan_Control_1,OUTPUT);
+//     pinMode(Fan_Control_2,OUTPUT);
+//     pinMode(Peltier_1,OUTPUT);
+//     pinMode(Peltier_2,OUTPUT);
+//     pinMode(SDA,OUTPUT);
+//     pinMode(SCL,OUTPUT);
 
-    // defines the inputs
-    pinMode(Water_Level,INPUT);
-    pinMode(LDR_1,INPUT); 
-    pinMode(LDR_2,INPUT);
+//     // defines the inputs
+//     pinMode(Water_Level,INPUT);
+//     pinMode(LDR_1,INPUT); 
+//     pinMode(LDR_2,INPUT);
 
     
     
-    AM2320Serial.begin(9600);
+//     AM2320Serial.begin(9600);
 
-    Wire.begin();
-    Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, Led_B, NEO_GRB + NEO_KHZ800);
+//     Wire.begin();
+//     Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, Led_B, NEO_GRB + NEO_KHZ800);
 
-    // MHZ19 myMHZ19;                               
-    // SoftwareSerial mySerial(CO2_RX, CO2_TX); 
+//     // MHZ19 myMHZ19;                               
+//     // SoftwareSerial mySerial(CO2_RX, CO2_TX); 
     
 
-    //Pwm pinnen instellen
-    ledcAttachPin(Pomp_Voeding, PWM1_Ch);
-    ledcSetup(PWM1_Ch, PWM1_Freq, PWM1_Res);
+//     //Pwm pinnen instellen
+//     ledcAttachPin(Pomp_Voeding, PWM1_Ch);
+//     ledcSetup(PWM1_Ch, PWM1_Freq, PWM1_Res);
 
-    ledcAttachPin(Pomp_Water, PWM2_Ch);
-    ledcSetup(PWM2_Ch, PWM1_Freq, PWM1_Res);
+//     ledcAttachPin(Pomp_Water, PWM2_Ch);
+//     ledcSetup(PWM2_Ch, PWM1_Freq, PWM1_Res);
 
                       
-    //myMHZ19.begin(mySerial);
-    //MHZ19PWM mhz(2, MHZ_ASYNC_MODE);
-    //mhz.useLimit(5000);
+//     //myMHZ19.begin(mySerial);
+//     //MHZ19PWM mhz(2, MHZ_ASYNC_MODE);
+//     //mhz.useLimit(5000);
 
-}
+// }
 
-void AIGB:: Time(){
-    int t=1;
+// void AIGB:: Time(){
+//     int t=1;
    
-}
+// }
 
-void AIGB::Calibrate(){
-    //Time();
-    if (AIGB::aigb_data->Profile_user<=0){
-    bool day;
+// void AIGB::Calibrate(){
+//     //Time();
+//     if (AIGB::aigb_data->Profile_user<=0){
+//     bool day;
     
-    if (20 > t && t> 7){
-        day=true;
-        AIGB::aigb_data->temp_setting_current=AIGB::aigb_data->temp_setting_inside_day;
-       AIGB::aigb_data->hum_setting_current=AIGB::aigb_data->Hum_setting_inside_day;
-    }
+//     if (20 > t && t> 7){
+//         day=true;
+//         AIGB::aigb_data->temp_setting_current=AIGB::aigb_data->temp_setting_inside_day;
+//        AIGB::aigb_data->hum_setting_current=AIGB::aigb_data->Hum_setting_inside_day;
+//     }
 
-    else{
-        day=false;
+//     else{
+//         day=false;
         
-        AIGB::aigb_data->temp_setting_current=AIGB::aigb_data->temp_setting_inside_night;
-        AIGB::aigb_data->hum_setting_current=AIGB::aigb_data->Hum_setting_inside_night;
-    }
-    }
-    else{
-        //profile is set by user
-    }
-    // check waterlevel
-    Get_water();
+//         AIGB::aigb_data->temp_setting_current=AIGB::aigb_data->temp_setting_inside_night;
+//         AIGB::aigb_data->hum_setting_current=AIGB::aigb_data->Hum_setting_inside_night;
+//     }
+//     }
+//     else{
+//         //profile is set by user
+//     }
+//     // check waterlevel
+//     Get_water();
     
-    // check humidity
-    Get_Hum();
+//     // check humidity
+//     Get_Hum();
 
-    // check light
-    Get_LDR();
+//     // check light
+//     Get_LDR();
 
-    Get_Co2();
+//     Get_Co2();
     
-    //Control();
+//     //Control();
     
-}
-void AIGB::Control(){
+// }
+// void AIGB::Control(){
     
-    //settings instellen
+//     //settings instellen
    
      
-    //check settings (looks if the settings are still compatible with the time of day)
-    // temp difference
-    Temp_Dif = AIGB::aigb_data->temp_setting_current-Temp_In;
-    Hum_Dif = AIGB::aigb_data->hum_setting_current-Hum_In;
-    if (-1<Temp_Dif&& Temp_Dif>1){
-        //temp control
-    }
+//     //check settings (looks if the settings are still compatible with the time of day)
+//     // temp difference
+//     Temp_Dif = AIGB::aigb_data->temp_setting_current-Temp_In;
+//     Hum_Dif = AIGB::aigb_data->hum_setting_current-Hum_In;
+//     if (-1<Temp_Dif&& Temp_Dif>1){
+//         //temp control
+//     }
 
-    else{
-        // keep Temp as it is
-    }
+//     else{
+//         // keep Temp as it is
+//     }
 
-    if (-1<Hum_Dif && Hum_Dif>1){
-        //huminity control
-        Moisture();
-    }
+//     if (-1<Hum_Dif && Hum_Dif>1){
+//         //huminity control
+//         Moisture();
+//     }
 
-    else{
-        digitalWrite(Vernevelaar,LOW);
-    }
+//     else{
+//         digitalWrite(Vernevelaar,LOW);
+//     }
     
-    LED();
+//     LED();
   
     
-}
+// }
 
-void AIGB::LED(){
-    if (AIGB::aigb_data->water_level_reading=0){
-        Led_Cycle=200;
-        for (int i=0;i<=Led_Cycle;i++){
-            if (Led_on==1 && i<=Led_Cycle){
-                Led_on=0;
-                digitalWrite(Led , HIGH);
-                i=0;
-            }
-            else if(Led_on==0 && i<=Led_Cycle){
-                Led_on=1;
-                digitalWrite(Led ,LOW);
-                i=0;
-            }
-        }
-    }
-}
+// void AIGB::LED(){
+//     if (AIGB::aigb_data->water_level_reading=0){
+//         Led_Cycle=200;
+//         for (int i=0;i<=Led_Cycle;i++){
+//             if (Led_on==1 && i<=Led_Cycle){
+//                 Led_on=0;
+//                 digitalWrite(Led , HIGH);
+//                 i=0;
+//             }
+//             else if(Led_on==0 && i<=Led_Cycle){
+//                 Led_on=1;
+//                 digitalWrite(Led ,LOW);
+//                 i=0;
+//             }
+//         }
+//     }
+// }
 
-void AIGB::Moisture(){
-    digitalWrite(Vernevelaar,HIGH);
-} 
+// void AIGB::Moisture(){
+//     digitalWrite(Vernevelaar,HIGH);
+// } 
 
-// has to be tested if it function 
-void AIGB::Get_Co2(){
+// // has to be tested if it function 
+// void AIGB::Get_Co2(){
     
-    // Start serial in setup routine
-    do{
-    th = pulseIn(CO2_PWM, HIGH, 1004000) / 1000;
-    tl = 1004 - th;
-    AIGB::aigb_data->co2_current_inside = 2000 * (th-2)/(th+tl-4);
-  } while (th == 0);
-    }
+//     // Start serial in setup routine
+//     do{
+//     th = pulseIn(CO2_PWM, HIGH, 1004000) / 1000;
+//     tl = 1004 - th;
+//     AIGB::aigb_data->co2_current_inside = 2000 * (th-2)/(th+tl-4);
+//   } while (th == 0);
+//     }
    
 
    
     
 
-void AIGB::Get_Hum(){
-    //get the value of the two huminity sensors
-    AIGB::aigb_data->Hum_reading_inside=th_1.h;
-    AIGB::aigb_data->Hum_reading_outside=th_2.h;
+// void AIGB::Get_Hum(){
+//     //get the value of the two huminity sensors
+//     AIGB::aigb_data->Hum_reading_inside=th_1.h;
+//     AIGB::aigb_data->Hum_reading_outside=th_2.h;
     
-    } 
+//     } 
 
-void AIGB::Get_Temp(){
-    //get the value of the temp according to the am2320
-    AIGB::aigb_data->temp_reading_inside=th_1.t;
-    AIGB::aigb_data->temp_reading_outside=th_2.t;
-}
+// void AIGB::Get_Temp(){
+//     //get the value of the temp according to the am2320
+//     AIGB::aigb_data->temp_reading_inside=th_1.t;
+//     AIGB::aigb_data->temp_reading_outside=th_2.t;
+// }
 
     
-void AIGB:: Get_water(){
-    AIGB::aigb_data->water_level_reading=   2  ;
-}
+// void AIGB:: Get_water(){
+//     AIGB::aigb_data->water_level_reading=   2  ;
+// }
 
-void AIGB:: Get_LDR(){
-    ldr_1=analogRead(LDR_1);
-    ldr_2=analogRead(LDR_2);
-    AIGB::aigb_data->light_reading_1=ldr_01;
-    AIGB::aigb_data->light_reading_2=ldr_02;
- }
+// void AIGB:: Get_LDR(){
+//     ldr_1=analogRead(LDR_1);
+//     ldr_2=analogRead(LDR_2);
+//     AIGB::aigb_data->light_reading_1=ldr_01;
+//     AIGB::aigb_data->light_reading_2=ldr_02;
+//  }
 
-// lets servo one move
-void AIGB:: Servo_one(){
-    MyServo1.write(180);
-    delay(15);
-    MyServo1.write(179);
-    delay(15);
-    MyServo1.write(175);
+// // lets servo one move
+// void AIGB:: Servo_one(){
+//     MyServo1.write(180);
+//     delay(15);
+//     MyServo1.write(179);
+//     delay(15);
+//     MyServo1.write(175);
   
-}
+// }
 
-// lets servo two move
-void AIGB:: Servo_two(){
-    MyServo2.write(90);
-    delay (200);
-    MyServo2.write(180);
-    delay (200);
-    MyServo2.write(0);
-    delay(200); 
-}
+// // lets servo two move
+// void AIGB:: Servo_two(){
+//     MyServo2.write(90);
+//     delay (200);
+//     MyServo2.write(180);
+//     delay (200);
+//     MyServo2.write(0);
+//     delay(200); 
+// }
 
-void AIGB::Water_Con(){
+// void AIGB::Water_Con(){
     
-    ledcWrite(PWM2_Ch, PWM2_DutyCycle);
-}
+//     ledcWrite(PWM2_Ch, PWM2_DutyCycle);
+// }
 
-void AIGB::Food_Con(){
-    //heel kort aan
-    if (AIGB::aigb_data->feed_interval_setting<food_timer){
+// void AIGB::Food_Con(){
+//     //heel kort aan
+//     if (AIGB::aigb_data->feed_interval_setting<food_timer){
         
-        ledcWrite(PWM1_Ch, PWM1_DutyCycle);
-    }
-    else{
-        ledcWrite(PWM1_Ch, 0);
-    }
-}
+//         ledcWrite(PWM1_Ch, PWM1_DutyCycle);
+//     }
+//     else{
+//         ledcWrite(PWM1_Ch, 0);
+//     }
+// }
